ce036ebf5c7da707e2bd0028a93d035b4c468fa7
Natallia Lantsuntsevich <nlantsuntsevich@sebrands.com>
2023-10-04 17:13:26 +0200

    updates

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,37 +1,37 @@
 
-function do_work()
-{
-    gapi.client.photoslibrary.sharedAlbums.list()
-    .then(function(response) {
-        const sharedAlbumId = response.result.sharedAlbums[0].id;
-        return gapi.client.photoslibrary.mediaItems.search({
-            albumId: sharedAlbumId,
-        });
-    })
-    .then(function(response) {
-        const mediaItems = response.result.mediaItems;
-        // Process and display the images on your website
-    });
+// function do_work()
+// {
+//     gapi.client.photoslibrary.sharedAlbums.list()
+//     .then(function(response) {
+//         const sharedAlbumId = response.result.sharedAlbums[0].id;
+//         return gapi.client.photoslibrary.mediaItems.search({
+//             albumId: sharedAlbumId,
+//         });
+//     })
+//     .then(function(response) {
+//         const mediaItems = response.result.mediaItems;
+//         // Process and display the images on your website
+//     });
 
-    gapi.client.photoslibrary.mediaItems.search({
-        albumId: sharedAlbumId,
-    })
-    .then(function(response) {
-        const mediaItems = response.result.mediaItems;
-        const imageContainer = document.getElementById('image-container'); // Replace with your container element ID
+//     gapi.client.photoslibrary.mediaItems.search({
+//         albumId: sharedAlbumId,
+//     })
+//     .then(function(response) {
+//         const mediaItems = response.result.mediaItems;
+//         const imageContainer = document.getElementById('image-container'); // Replace with your container element ID
     
-        mediaItems.forEach(function(mediaItem) {
-            const baseUrl = mediaItem.baseUrl;
-            const imageUrl = baseUrl + '=w500'; // Adjust the desired width as needed
+//         mediaItems.forEach(function(mediaItem) {
+//             const baseUrl = mediaItem.baseUrl;
+//             const imageUrl = baseUrl + '=w500'; // Adjust the desired width as needed
     
-            const imageElement = document.createElement('img');
-            imageElement.src = imageUrl;
-            imageElement.alt = 'Image';
+//             const imageElement = document.createElement('img');
+//             imageElement.src = imageUrl;
+//             imageElement.alt = 'Image';
             
-            imageContainer.appendChild(imageElement);
-        });
-    })
-    .catch(function(error) {
-        console.error('Error fetching media items:', error);
-    });
-}
+//             imageContainer.appendChild(imageElement);
+//         });
+//     })
+//     .catch(function(error) {
+//         console.error('Error fetching media items:', error);
+//     });
+// }
