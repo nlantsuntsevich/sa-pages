fb0b4a8bf6a0f440306f9c97988d749263fb3c70
natalia <n.lantsuntsevich@gmail.com>
2024-03-31 17:00:31 +0200

    1111

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,37 +1,13 @@
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
-function do_work()
-{
-    // gapi.client.photoslibrary.sharedAlbums.list()
-    // .then(function(response) {
-    //     const sharedAlbumId = response.result.sharedAlbums[0].id;
-    //     return gapi.client.photoslibrary.mediaItems.search({
-    //         albumId: sharedAlbumId,
-    //     });
-    // })
-    // .then(function(response) {
-    //     const mediaItems = response.result.mediaItems;
-    //     // Process and display the images on your website
-    // });
-
-    // gapi.client.photoslibrary.mediaItems.search({
-    //     albumId: sharedAlbumId,
-    // })
-    // .then(function(response) {
-    //     const mediaItems = response.result.mediaItems;
-    //     const imageContainer = document.getElementById('image-container'); // Replace with your container element ID
-    
-    //     mediaItems.forEach(function(mediaItem) {
-    //         const baseUrl = mediaItem.baseUrl;
-    //         const imageUrl = baseUrl + '=w500'; // Adjust the desired width as needed
-    
-    //         const imageElement = document.createElement('img');
-    //         imageElement.src = imageUrl;
-    //         imageElement.alt = 'Image';
-            
-    //         imageContainer.appendChild(imageElement);
-    //     });
-    // })
-    // .catch(function(error) {
-    //     console.error('Error fetching media items:', error);
-    // });
-}
+// init Masonry
+var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    columnWidth: '.grid-sizer'
+  });
+  // layout Masonry after each image loads
+  $grid.imagesLoaded().progress( function() {
+    $grid.masonry();
+  });  
+  
